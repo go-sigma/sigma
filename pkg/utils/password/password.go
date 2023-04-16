77e60b0f90c5c462f264331d404566a94b11f8ad
Tosone <i@tosone.cn>
2023-04-16 19:53:29 +0800

    :sparkles: Add helm chart (#40)

--- pkg/utils/password/password.go
+++ pkg/utils/password/password.go
@@ -15,29 +15,7 @@
 package password
 
 import (
-	"crypto/rand"
-	"crypto/subtle"
-	"encoding/base64"
-	"errors"
-	"fmt"
-	"strings"
-
-	"golang.org/x/crypto/argon2"
-)
-
-var (
-	// ErrInvalidHash in returned by ComparePasswordAndHash if the provided
-	// hash isn't in the expected format.
-	ErrInvalidHash = errors.New("argon2id: hash is not in the correct format")
-
-	// ErrIncompatibleVariant is returned by ComparePasswordAndHash if the
-	// provided hash was created using a unsupported variant of Argon2.
-	// Currently only argon2id is supported by this package.
-	ErrIncompatibleVariant = errors.New("argon2id: incompatible variant of argon2")
-
-	// ErrIncompatibleVersion is returned by ComparePasswordAndHash if the
-	// provided hash was created using a different version of Argon2.
-	ErrIncompatibleVersion = errors.New("argon2id: incompatible version of argon2")
+	"golang.org/x/crypto/bcrypt"
 )
 
 // Password is an interface for password hashing
@@ -46,129 +24,34 @@ type Password interface {
 	// Hash returns a hashed password
 	Hash(pwd string) (string, error)
 	// Verify compares a password with a hashed password
-	Verify(pwd, hash string) (bool, error)
+	Verify(pwd, hash string) bool
 }
 
 type password struct {
-	params Params
+	cost int
 }
 
-// Params describes the input parameters used by the Argon2id algorithm. The
-// Memory and Iterations parameters control the computational cost of hashing
-// the password. The higher these figures are, the greater the cost of generating
-// the hash and the longer the runtime. It also follows that the greater the cost
-// will be for any attacker trying to guess the password. If the code is running
-// on a machine with multiple cores, then you can decrease the runtime without
-// reducing the cost by increasing the Parallelism parameter. This controls the
-// number of threads that the work is spread across. Important note: Changing the
-// value of the Parallelism parameter changes the hash output.
-//
-// For guidance and an outline process for choosing appropriate parameters see
-// https://tools.ietf.org/html/draft-irtf-cfrg-argon2-04#section-4
-type Params struct {
-	// The amount of memory used by the algorithm (in kilobytes).
-	Memory uint32
-
-	// The number of iterations over the memory.
-	Iterations uint32
-
-	// The number of threads (or lanes) used by the algorithm.
-	// Recommended value is between 1 and runtime.NumCPU().
-	Parallelism uint8
-
-	// Length of the random salt. 16 bytes is recommended for password hashing.
-	SaltLength uint32
-
-	// Length of the generated key. 16 bytes or more is recommended.
-	KeyLength uint32
-}
-
-// DefaultParams is the default parameters for the Argon2 algorithm.
-var DefaultParams = Params{
-	Memory:      64 * 1024,
-	Iterations:  1,
-	Parallelism: 2,
-	SaltLength:  16,
-	KeyLength:   32,
-}
-
-// New returns a new Password
-func New(params ...Params) Password {
-	param := DefaultParams
-	if len(params) > 0 {
-		param = params[0]
+// New returns a new password instance
+func New(costs ...int) Password {
+	var cost = bcrypt.DefaultCost
+	if len(costs) > 0 {
+		cost = costs[0]
 	}
 	return &password{
-		params: param,
+		cost: cost,
 	}
 }
 
 // Hash returns a hashed password
 func (p *password) Hash(pwd string) (string, error) {
-	salt := make([]byte, p.params.SaltLength)
-	_, err := rand.Read(salt)
+	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), p.cost)
 	if err != nil {
 		return "", err
 	}
-
-	key := argon2.IDKey([]byte(pwd), salt, p.params.Iterations, p.params.Memory, p.params.Parallelism, p.params.KeyLength)
-
-	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
-	b64Key := base64.RawStdEncoding.EncodeToString(key)
-
-	hash := fmt.Sprintf("$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s", argon2.Version, p.params.Memory, p.params.Iterations, p.params.Parallelism, b64Salt, b64Key)
-	return hash, nil
+	return string(hash), nil
 }
 
 // Verify compares a password with a hashed password
-func (p *password) Verify(pwd, hash string) (bool, error) {
-	vals := strings.Split(hash, "$")
-	if len(vals) != 6 {
-		return false, ErrInvalidHash
-	}
-
-	if vals[1] != "argon2id" {
-		return false, ErrIncompatibleVariant
-	}
-
-	var version int
-	_, err := fmt.Sscanf(vals[2], "v=%d", &version)
-	if err != nil {
-		return false, err
-	}
-	if version != argon2.Version {
-		return false, ErrIncompatibleVersion
-	}
-
-	params := Params{}
-	_, err = fmt.Sscanf(vals[3], "m=%d,t=%d,p=%d", &params.Memory, &params.Iterations, &params.Parallelism)
-	if err != nil {
-		return false, err
-	}
-
-	salt, err := base64.RawStdEncoding.Strict().DecodeString(vals[4])
-	if err != nil {
-		return false, err
-	}
-	params.SaltLength = uint32(len(salt))
-
-	key, err := base64.RawStdEncoding.Strict().DecodeString(vals[5])
-	if err != nil {
-		return false, err
-	}
-	params.KeyLength = uint32(len(key))
-
-	otherKey := argon2.IDKey([]byte(pwd), salt, params.Iterations, params.Memory, params.Parallelism, params.KeyLength)
-
-	keyLen := int32(len(key))
-	otherKeyLen := int32(len(otherKey))
-
-	if subtle.ConstantTimeEq(keyLen, otherKeyLen) == 0 {
-		return false, nil
-	}
-	if subtle.ConstantTimeCompare(key, otherKey) == 1 {
-		return true, nil
-	}
-
-	return false, nil
+func (p *password) Verify(pwd, hash string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(pwd)) == nil
 }
