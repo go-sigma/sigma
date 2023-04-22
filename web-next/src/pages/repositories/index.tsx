44436a60f074a0899ff235dfda2195e46ea1888d
Tosone <i@tosone.cn>
2023-04-23 00:43:36 +0800

    :sparkles: Add nextjs as new fe (#47)
    * :sparkles: Add nextjs as new fe
    
    * :sparkles: Update go-enum

---
+++ web-next/src/pages/repositories/index.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return <>
+    repository
+  </>
+}
