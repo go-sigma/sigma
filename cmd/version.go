f838afd969ca94a4ed362f98211bebd01868c881
Tosone <i@tosone.cn>
2023-09-12 18:02:11 +0800

    :recycle: Rename ximager to sigma

--- cmd/version.go
+++ cmd/version.go
@@ -22,30 +22,32 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// You can copy & paste this ascii graphic and use it e.g. as mail signature
+// Font: doh   Reflection: no   Adjustment: left   Stretch: no      Width: 280	 Text: sigma
 const banner = `
-XXXXXXX       XXXXXXIIIIIIIIII
-X:::::X       X:::::I::::::::I
-X:::::X       X:::::I::::::::I
-X::::::X     X::::::II::::::II
-XXX:::::X   X:::::XXX I::::I    mmmmmmm    mmmmmmm    aaaaaaaaaaaaa    ggggggggg   ggggg   eeeeeeeeeeee   rrrrr   rrrrrrrrr
-   X:::::X X:::::X    I::::I  mm:::::::m  m:::::::mm  a::::::::::::a  g:::::::::ggg::::g ee::::::::::::ee r::::rrr:::::::::r
-    X:::::X:::::X     I::::I m::::::::::mm::::::::::m aaaaaaaaa:::::ag:::::::::::::::::ge::::::eeeee:::::er:::::::::::::::::r
-     X:::::::::X      I::::I m::::::::::::::::::::::m          a::::g::::::ggggg::::::ge::::::e     e:::::rr::::::rrrrr::::::r
-     X:::::::::X      I::::I m:::::mmm::::::mmm:::::m   aaaaaaa:::::g:::::g     g:::::ge:::::::eeeee::::::er:::::r     r:::::r
-    X:::::X:::::X     I::::I m::::m   m::::m   m::::m aa::::::::::::g:::::g     g:::::ge:::::::::::::::::e r:::::r     rrrrrrr
-   X:::::X X:::::X    I::::I m::::m   m::::m   m::::ma::::aaaa::::::g:::::g     g:::::ge::::::eeeeeeeeeee  r:::::r
-XXX:::::X   X:::::XXX I::::I m::::m   m::::m   m::::a::::a    a:::::g::::::g    g:::::ge:::::::e           r:::::r
-X::::::X     X::::::II::::::Im::::m   m::::m   m::::a::::a    a:::::g:::::::ggggg:::::ge::::::::e          r:::::r
-X:::::X       X:::::I::::::::m::::m   m::::m   m::::a:::::aaaa::::::ag::::::::::::::::g e::::::::eeeeeeee  r:::::r
-X:::::X       X:::::I::::::::m::::m   m::::m   m::::ma::::::::::aa:::agg::::::::::::::g  ee:::::::::::::e  r:::::r
-XXXXXXX       XXXXXXIIIIIIIIImmmmmm   mmmmmm   mmmmmm aaaaaaaaaa  aaaa  gggggggg::::::g    eeeeeeeeeeeeee  rrrrrrr
-                                                                                g:::::g
-                                                                    gggggg      g:::::g
-                                                                    g:::::gg   gg:::::g
-                                                                     g::::::ggg:::::::g
-                                                                      gg:::::::::::::g
-                                                                        ggg::::::ggg
-                                                                           gggggg`
+                   iiii
+                  i::::i
+                   iiii
+
+    ssssssssss   iiiiiii    ggggggggg   ggggg   mmmmmmm    mmmmmmm     aaaaaaaaaaaaa
+  ss::::::::::s  i:::::i   g:::::::::ggg::::g mm:::::::m  m:::::::mm   a::::::::::::a
+ss:::::::::::::s  i::::i  g:::::::::::::::::gm::::::::::mm::::::::::m  aaaaaaaaa:::::a
+s::::::ssss:::::s i::::i g::::::ggggg::::::ggm::::::::::::::::::::::m           a::::a
+ s:::::s  ssssss  i::::i g:::::g     g:::::g m:::::mmm::::::mmm:::::m    aaaaaaa:::::a
+   s::::::s       i::::i g:::::g     g:::::g m::::m   m::::m   m::::m  aa::::::::::::a
+      s::::::s    i::::i g:::::g     g:::::g m::::m   m::::m   m::::m a::::aaaa::::::a
+ssssss   s:::::s  i::::i g::::::g    g:::::g m::::m   m::::m   m::::ma::::a    a:::::a
+s:::::ssss::::::si::::::ig:::::::ggggg:::::g m::::m   m::::m   m::::ma::::a    a:::::a
+s::::::::::::::s i::::::i g::::::::::::::::g m::::m   m::::m   m::::ma:::::aaaa::::::a
+ s:::::::::::ss  i::::::i  gg::::::::::::::g m::::m   m::::m   m::::m a::::::::::aa:::a
+  sssssssssss    iiiiiiii    gggggggg::::::g mmmmmm   mmmmmm   mmmmmm  aaaaaaaaaa  aaaa
+                                     g:::::g
+                         gggggg      g:::::g
+                         g:::::gg   gg:::::g
+                          g::::::ggg:::::::g
+                           gg:::::::::::::g
+                             ggg::::::ggg
+                                gggggg`
 
 var (
 	version   = ""
@@ -56,7 +58,7 @@ var (
 // versionCmd represents the worker command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Show version of XImager",
+	Short: "Show version of sigma",
 	Run: func(_ *cobra.Command, _ []string) {
 		color.Cyan(banner)
 		fmt.Printf("Version:     %s\n", version)
