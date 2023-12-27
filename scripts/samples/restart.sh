d856c2941546dcbeba46f42c1d3602b355732f5d
Tosone <i@tosone.cn>
2023-12-27 11:50:00 +0800

    :hammer: Update offline package script

--- scripts/samples/restart.sh
+++ scripts/samples/restart.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+DOCKER=${DOCKER:-docker}
+SIGMA_VOLUME=${SIGMA_VOLUME:-$PWD}
+
+if [ "$SIGMA_VOLUME" != "$PWD" ]; then
+  cp -rf sigma.tar.gz "$SIGMA_VOLUME"
+  cp -rf docker-compose.yml "$SIGMA_VOLUME"
+  cp -rf samples "$SIGMA_VOLUME"
+fi
+
 "$DOCKER" exec -it sigma-dind \
-  cd /app && docker load -i sigma.tar.gz && \
+  sh -c 'cd /app && docker load -i sigma.tar.gz && \
   docker compose --project-name sigma down && \
-  docker compose --project-name sigma -f /app/docker-compose.yml up --detach
+  docker compose --project-name sigma -f /app/docker-compose.yml up --detach'
