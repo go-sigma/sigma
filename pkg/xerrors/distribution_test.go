01781a4c33ff7371d78bf8c8d5cfe535ec8ae0d4
Tosone <i@tosone.cn>
2024-02-29 00:26:17 +0800

    :green_heart: Add errors test case (#322)

--- pkg/xerrors/distribution_test.go
+++ pkg/xerrors/distribution_test.go
@@ -16,6 +16,7 @@ package xerrors
 
 import (
 	"bytes"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -38,4 +39,38 @@ func TestNewDSError(t *testing.T) {
 func TestDSErrCode(t *testing.T) {
 	e := ErrCode{Code: "title", Title: "message"}
 	assert.Equal(t, "title: message", e.Error())
+}
+
+func TestGenDSErrCodeResourceSizeQuotaExceedNamespace(t *testing.T) {
+	assert.Equal(t, "requested access to the size quota is exceed, namespace(library) size quota 100 B/100 B(100%), increasing size is 10 B", GenDSErrCodeResourceSizeQuotaExceedNamespace("library", 100, 100, 10).Title)
+}
+
+func TestGenDSErrCodeResourceSizeQuotaExceedRepository(t *testing.T) {
+	assert.Equal(t, "requested access to the size quota is exceed, repository(library/alpine) size quota 100 B/100 B(100%), increasing size is 10 B", GenDSErrCodeResourceSizeQuotaExceedRepository("library/alpine", 100, 100, 10).Title)
+}
+
+func TestGenDSErrCodeResourceCountQuotaExceedRepository(t *testing.T) {
+	assert.Equal(t, "requested access to the resource count quota is exceed, repository(library/alpine) tag count quota is 10", GenDSErrCodeResourceCountQuotaExceedRepository("library/alpine", 10).Title)
+}
+
+func TestGenDSErrCodeResourceCountQuotaExceedNamespaceRepository(t *testing.T) {
+	assert.Equal(t, "requested access to the resource count quota is exceed, namespace(library) repository count quota is 10", GenDSErrCodeResourceCountQuotaExceedNamespaceRepository("library", 10).Title)
+}
+
+func TestGenDSErrCodeResourceCountQuotaExceedNamespaceTag(t *testing.T) {
+	assert.Equal(t, "requested access to the resource count quota is exceed, namespace(library) tag count quota is 10", GenDSErrCodeResourceCountQuotaExceedNamespaceTag("library", 10).Title)
+}
+
+func TestGenDSErrCodeResourceNotFound(t *testing.T) {
+	assert.Equal(t, "Not found", GenDSErrCodeResourceNotFound(errors.New("Not found")).Title)
+}
+
+func TestRound(t *testing.T) {
+	assert.Equal(t, 1, round(1.4))
+	assert.Equal(t, 2, round(1.6))
+}
+
+func TestToFixed(t *testing.T) {
+	assert.Equal(t, 1.5, toFixed(1.51, 1))
+	assert.Equal(t, 1.51, toFixed(1.511, 2))
 }
