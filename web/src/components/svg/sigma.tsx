58da0456b0a20d57d93831a0fc15aa1628982f3f
Tosone <i@tosone.cn>
2024-05-06 18:01:47 +0800

    :sparkles: Add icon on login page

---
+++ web/src/components/svg/sigma.tsx
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2023 sigma
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export default function () {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 190 190">
+      <circle cx="95" cy="95" r="95" fill="#f99d1c" />
+      <circle cx="95" cy="95" r="85" fill="#cd4b27" />
+      <circle cx="95" cy="95" r="75" fill="#5c2e0e" />
+      <path d="m71.32,125.88v-5.28l23.52-29.09v4.99l-22.56-31.58v-3.36h7.01l21.6,30.24-23.71,29.95-2.5-3.55h39.55l-5.28,4.8,3.65-17.28h5.76l-.77,20.16h-46.27Zm36-64.32l5.28,4.8h-37.05v-4.8h40.41l.77,17.09h-5.76l-3.65-17.09Z" fill="#f99d1c" />
+    </svg>
+  );
+}
