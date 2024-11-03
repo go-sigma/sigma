8c8c315850b600351b19f064a5c0a9ce04bfcadf
Tosone <i@tosone.cn>
2024-11-03 13:14:12 +0800

    :sparkles: Add redis in github action

--- scripts/run_redis.sh
+++ scripts/run_redis.sh
@@ -2,11 +2,11 @@
 
 DOCKER=${DOCKER:-docker}
 
-"$DOCKER" run -it \
+"$DOCKER" run \
   --name sigma-redis \
   -p 6379:6379 -d --rm \
   --health-cmd "redis-cli -a sigma ping || exit 1" \
   --health-interval 10s \
   --health-timeout 5s \
   --health-retries 10 \
-  redis:7.0-alpine --requirepass sigma
+  redis:7-alpine --requirepass sigma
