218346ecf066345690e655a7f6b730b1b400edda
Tosone <i@tosone.cn>
2024-12-11 01:43:00 +0800

    :sparkles: Update oauth2 ut

--- pkg/handlers/oauth2/oauth2_clientid_test.go
+++ pkg/handlers/oauth2/oauth2_clientid_test.go
@@ -14,97 +14,93 @@
 
 package oauth2
 
-// import (
-// 	"net/http"
-// 	"net/http/httptest"
-// 	"testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-// 	"github.com/labstack/echo/v4"
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/tidwall/gjson"
-// 	"go.uber.org/mock/gomock"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/tidwall/gjson"
+	"go.uber.org/dig"
 
-// 	"github.com/go-sigma/sigma/pkg/configs"
-// 	"github.com/go-sigma/sigma/pkg/dal"
-// 	"github.com/go-sigma/sigma/pkg/tests"
-// 	"github.com/go-sigma/sigma/pkg/types/enums"
-// 	"github.com/go-sigma/sigma/pkg/validators"
-// )
+	"github.com/go-sigma/sigma/pkg/configs"
+	"github.com/go-sigma/sigma/pkg/dal/dao"
+	"github.com/go-sigma/sigma/pkg/tests"
+	"github.com/go-sigma/sigma/pkg/types/enums"
+	"github.com/go-sigma/sigma/pkg/utils/token"
+	"github.com/go-sigma/sigma/pkg/validators"
+)
 
-// func TestGetVersion(t *testing.T) {
-// 	e := echo.New()
-// 	validators.Initialize(e)
-// 	assert.NoError(t, tests.Initialize(t))
-// 	assert.NoError(t, tests.DB.Init())
-// 	defer func() {
-// 		conn, err := dal.DB.DB()
-// 		assert.NoError(t, err)
-// 		assert.NoError(t, conn.Close())
-// 		assert.NoError(t, tests.DB.DeInit())
-// 	}()
+func TestClientID(t *testing.T) {
+	digCon := dig.New()
+	err := digCon.Provide(func() configs.Configuration {
+		return configs.Configuration{
+			Auth: configs.ConfigurationAuth{
+				Oauth2: configs.ConfigurationAuthOauth2{
+					Github: configs.ConfigurationAuthOauth2Github{
+						ClientID: "github_client_id",
+					},
+					Gitlab: configs.ConfigurationAuthOauth2Gitlab{
+						ClientID: "gitlab_client_id",
+					},
+					Gitea: configs.ConfigurationAuthOauth2Gitea{
+						ClientID: "gitea_client_id",
+					},
+				},
+			},
+		}
+	})
+	require.NoError(t, err)
+	require.NoError(t, digCon.Provide(func() token.Service { return nil }))
+	require.NoError(t, digCon.Provide(func() dao.UserServiceFactory { return nil }))
 
-// 	ctrl := gomock.NewController(t)
-// 	defer ctrl.Finish()
+	handler := handlerNew(digCon)
 
-// 	oauth2Handler, err := handlerNew(inject{
-// 		config: &configs.Configuration{
-// 			Auth: configs.ConfigurationAuth{
-// 				Jwt: configs.ConfigurationAuthJwt{
-// 					PrivateKey: privateKeyString,
-// 				},
-// 				Oauth2: configs.ConfigurationAuthOauth2{
-// 					Github: configs.ConfigurationAuthOauth2Github{
-// 						ClientID: "github_client_id",
-// 					},
-// 					Gitlab: configs.ConfigurationAuthOauth2Gitlab{
-// 						ClientID: "gitlab_client_id",
-// 					},
-// 					Gitea: configs.ConfigurationAuthOauth2Gitea{
-// 						ClientID: "gitea_client_id",
-// 					},
-// 				},
-// 			},
-// 		},
-// 	})
-// 	assert.NoError(t, err)
-
-// 	{
-// 		req := httptest.NewRequest(http.MethodGet, "/", nil)
-// 		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-// 		rec := httptest.NewRecorder()
-// 		c := e.NewContext(req, rec)
-// 		c.SetParamNames("provider")
-// 		c.SetParamValues(enums.ProviderGithub.String())
-// 		err = oauth2Handler.ClientID(c)
-// 		assert.NoError(t, err)
-// 		assert.Equal(t, http.StatusOK, c.Response().Status)
-// 		response := rec.Body.Bytes()
-// 		assert.Equal(t, "github_client_id", gjson.GetBytes(response, "client_id").String())
-// 	}
-// 	{
-// 		req := httptest.NewRequest(http.MethodGet, "/", nil)
-// 		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-// 		rec := httptest.NewRecorder()
-// 		c := e.NewContext(req, rec)
-// 		c.SetParamNames("provider")
-// 		c.SetParamValues(enums.ProviderGitlab.String())
-// 		err = oauth2Handler.ClientID(c)
-// 		assert.NoError(t, err)
-// 		assert.Equal(t, http.StatusOK, c.Response().Status)
-// 		response := rec.Body.Bytes()
-// 		assert.Equal(t, "gitlab_client_id", gjson.GetBytes(response, "client_id").String())
-// 	}
-// 	{
-// 		req := httptest.NewRequest(http.MethodGet, "/", nil)
-// 		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-// 		rec := httptest.NewRecorder()
-// 		c := e.NewContext(req, rec)
-// 		c.SetParamNames("provider")
-// 		c.SetParamValues(enums.ProviderGitea.String())
-// 		err = oauth2Handler.ClientID(c)
-// 		assert.NoError(t, err)
-// 		assert.Equal(t, http.StatusOK, c.Response().Status)
-// 		response := rec.Body.Bytes()
-// 		assert.Equal(t, "gitea_client_id", gjson.GetBytes(response, "client_id").String())
-// 	}
-// }
+	{
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		e := tests.NewEcho()
+		validators.Initialize(e)
+		c := e.NewContext(req, rec)
+		c.SetParamNames("provider")
+		c.SetParamValues(enums.ProviderGithub.String())
+		err := handler.ClientID(c)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, c.Response().Status)
+		response := rec.Body.Bytes()
+		assert.Equal(t, "github_client_id", gjson.GetBytes(response, "client_id").String())
+	}
+	{
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		e := tests.NewEcho()
+		validators.Initialize(e)
+		c := e.NewContext(req, rec)
+		c.SetParamNames("provider")
+		c.SetParamValues(enums.ProviderGitlab.String())
+		err := handler.ClientID(c)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, c.Response().Status)
+		response := rec.Body.Bytes()
+		assert.Equal(t, "gitlab_client_id", gjson.GetBytes(response, "client_id").String())
+	}
+	{
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		e := tests.NewEcho()
+		validators.Initialize(e)
+		c := e.NewContext(req, rec)
+		c.SetParamNames("provider")
+		c.SetParamValues(enums.ProviderGitea.String())
+		err := handler.ClientID(c)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, c.Response().Status)
+		response := rec.Body.Bytes()
+		assert.Equal(t, "gitea_client_id", gjson.GetBytes(response, "client_id").String())
+	}
+}
